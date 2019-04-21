159320e055c319739117034db0098d1d13512e01
Ekaterina Kuzmina <49808760+ekaterinakuzmina@users.noreply.github.com>
2019-04-21 22:40:00 +0200

    Create checkSameElementExistence.py

---
+++ Challenges/checkSameElementExistence.py
@@ -0,0 +1,11 @@
+'''Given two sorted arrays of integers, check if there is at least one element which occurs in both arrays.
+
+Example
+
+For arr1 = [1, 2, 3, 5] and arr2 = [1, 4, 5], the output should be
+checkSameElementExistence(arr1, arr2) = true;
+For arr1 = [1, 3, 5] and arr2 = [-2, 0, 2, 4, 6], the output should be
+checkSameElementExistence(arr1, arr2) = false.'''
+
+def checkSameElementExistence(arr1, arr2):
+    return list(set(arr1) & set(arr2)) != []
