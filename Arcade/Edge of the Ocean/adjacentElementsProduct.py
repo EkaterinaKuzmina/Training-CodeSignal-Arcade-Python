302a0346504be90d4c8f4c19bce1230f83f06418
Ekaterina Kuzmina <49808760+ekaterinakuzmina@users.noreply.github.com>
2019-04-23 20:31:45 +0200

    Update adjacentElementsProduct.py

--- Arcade/Edge of the Ocean/adjacentElementsProduct.py
+++ Arcade/Edge of the Ocean/adjacentElementsProduct.py
@@ -1,12 +1,3 @@
-'''Given an array of integers, find the pair of adjacent elements that has the largest product and return that product.
-
-Example
-
-For inputArray = [3, 6, -2, -5, 7, 3], the output should be
-adjacentElementsProduct(inputArray) = 21.
-
-7 and 3 produce the largest product.'''
-
 def adjacentElementsProduct(inputArray):
     n = len(inputArray)
     maximum = -float("inf")
@@ -15,3 +6,10 @@ def adjacentElementsProduct(inputArray):
         if t > maximum:
             maximum = t
     return maximum
+
+'''Given an array of integers, find the pair of adjacent elements that has the largest product and return that product.
+
+Example
+For inputArray = [3, 6, -2, -5, 7, 3], the output should be
+adjacentElementsProduct(inputArray) = 21.
+7 and 3 produce the largest product.'''
