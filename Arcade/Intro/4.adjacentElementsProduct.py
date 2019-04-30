d534be5f0281eed8713a2bc9cd848ac6dc27e8cb
Ekaterina Kuzmina <49808760+ekaterinakuzmina@users.noreply.github.com>
2019-04-30 16:13:58 +0200

    Update 4.adjacentElementsProduct.py

--- Arcade/Intro/4.adjacentElementsProduct.py
+++ Arcade/Intro/4.adjacentElementsProduct.py
@@ -1,7 +1,7 @@
 def adjacentElementsProduct(inputArray):
     n = len(inputArray)
     maximum = -float("inf")
-    for i in range(0, n-1):
+    for i in range(n-1):
         t = inputArray[i]*inputArray[i+1]
         if t > maximum:
             maximum = t
