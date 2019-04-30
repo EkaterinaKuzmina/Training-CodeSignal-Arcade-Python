6ff4fc9d41687fd58d354a34e0f567645b6bddf1
Ekaterina Kuzmina <49808760+ekaterinakuzmina@users.noreply.github.com>
2019-04-30 16:12:37 +0200

    Create 4.adjacentElementsProduct.py

---
+++ Arcade/Intro/4.adjacentElementsProduct.py
@@ -0,0 +1,16 @@
+def adjacentElementsProduct(inputArray):
+    n = len(inputArray)
+    maximum = -float("inf")
+    for i in range(0, n-1):
+        t = inputArray[i]*inputArray[i+1]
+        if t > maximum:
+            maximum = t
+    return maximum
+
+'''Given an array of integers, find the pair of adjacent elements that has the largest product and return that product.
+
+Example
+For inputArray = [3, 6, -2, -5, 7, 3], the output should be
+adjacentElementsProduct(inputArray) = 21.
+
+7 and 3 produce the largest product.'''
