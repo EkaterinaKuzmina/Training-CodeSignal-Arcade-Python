0659be431109eeb806a3d4d3d049da919212dbe7
Ekaterina Kuzmina <49808760+ekaterinakuzmina@users.noreply.github.com>
2019-05-01 19:24:25 +0200

    Create 48.isDigit.py

---
+++ Arcade/Intro/48.isDigit.py
@@ -0,0 +1,14 @@
+def isDigit(symbol):
+    return symbol in ('1','2','3','4','5','6','7','8','9','0')
+    
+#or
+#def isDigit(symbol):
+#    return symbol.isdigit()
+
+'''Determine if the given character is a digit or not.
+
+Example
+For symbol = '0', the output should be
+isDigit(symbol) = true;
+For symbol = '-', the output should be
+isDigit(symbol) = false.'''
