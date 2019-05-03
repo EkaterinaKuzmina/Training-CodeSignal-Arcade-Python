1df4f49f10d77f40c49cd5c12f59f08de4385b67
Ekaterina Kuzmina <49808760+ekaterinakuzmina@users.noreply.github.com>
2019-05-03 18:17:30 +0200

    Create 54.sumUpNumbers.py

---
+++ Arcade/Intro/54.sumUpNumbers.py
@@ -0,0 +1,14 @@
+def sumUpNumbers(inputString):
+    newinputString = ''.join((ch if ch in '0123456789' else ' ') for ch in inputString)
+    listOfNumbers = [int(i) for i in newinputString.split()]
+    return sum(listOfNumbers)
+
+'''CodeMaster has just returned from shopping. 
+He scanned the check of the items he bought and gave the resulting string to Ratiorg to figure out the total number of purchased items. 
+Since Ratiorg is a bot he is definitely going to automate it, 
+so he needs a program that sums up all the numbers which appear in the given input.
+Help Ratiorg by writing a function that returns the sum of numbers that appear in the given inputString.
+
+Example
+For inputString = "2 apples, 12 oranges", the output should be
+sumUpNumbers(inputString) = 14.'''
