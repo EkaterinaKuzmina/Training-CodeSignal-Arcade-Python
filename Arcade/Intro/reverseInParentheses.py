f170afd6b3523cf1cd3a2409be218b4a96fa5558
Ekaterina Kuzmina <49808760+ekaterinakuzmina@users.noreply.github.com>
2019-05-06 21:40:47 +0200

    Create reverseInParentheses.py

---
+++ Arcade/Intro/reverseInParentheses.py
@@ -0,0 +1,25 @@
+def reverseInParentheses(inputString):
+    chs = list(inputString)
+    open_bracket_indexes = []
+    for i, c in enumerate(chs):
+        if c == '(':
+            open_bracket_indexes.append(i)
+        elif c == ')':
+            j = open_bracket_indexes.pop()
+            chs[j:i] = chs[i:j:-1]
+    return ''.join(c for c in chs if c not in '()')
+
+'''Write a function that reverses characters in (possibly nested) parentheses in the input string.
+
+Input strings will always be well-formed with matching ()s.
+
+Example
+For inputString = "(bar)", the output should be
+reverseInParentheses(inputString) = "rab";
+For inputString = "foo(bar)baz", the output should be
+reverseInParentheses(inputString) = "foorabbaz";
+For inputString = "foo(bar)baz(blim)", the output should be
+reverseInParentheses(inputString) = "foorabbazmilb";
+For inputString = "foo(bar(baz))blim", the output should be
+reverseInParentheses(inputString) = "foobazrabblim".
+Because "foo(bar(baz))blim" becomes "foo(barzab)blim" and then "foobazrabblim".'''
