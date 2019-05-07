0aed0b3110329a882bc6edd9832c2ab46dde9784
Ekaterina Kuzmina <49808760+ekaterinakuzmina@users.noreply.github.com>
2019-05-07 21:32:18 +0200

    Update 2.centuryFromYear.py

--- Arcade/Intro/2.centuryFromYear.py
+++ Arcade/Intro/2.centuryFromYear.py
@@ -4,7 +4,9 @@ def centuryFromYear(year):
     else:
         return int(year/100 + 1)
 
-'''Given a year, return the century it is in. The first century spans from the year 1 up to and including the year 100, the second - from the year 101 up to and including the year 200, etc.
+'''Given a year, return the century it is in. 
+The first century spans from the year 1 up to and including the year 100, 
+the second - from the year 101 up to and including the year 200, etc.
 
 Example
 For year = 1905, the output should be
